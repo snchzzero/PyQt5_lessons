1f576b2f972389d2e447a4fae6511f1dcbece32d
Евстафьев Александр Анатольевич <evstafev-aa@prima.local>
2022-12-06 10:04:18 +0300

    Ex_3.7

---
+++ lessons_3/Ex_3.7_send signal.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5.QtCore import pyqtSignal, QObject
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+# Когда мы кликаем на окне курсором мыши, посылается сигнал closeApp. Приложение завершается.
+class Communicate(QObject):
+    closeApp = pyqtSignal()  # cоздаём новый сигнал
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()  # возвращает родительский объект Example c классом
+        self.initUI()
+
+    def initUI(self):
+        self.c = Communicate()
+        self.c.closeApp.connect(self.close)  # сигнал closeApp присоединяется к слоту close() класса QMainWindow.
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Emit signal')
+        self.show()
+
+    def mousePressEvent(self, event):
+        self.c.closeApp.emit()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
