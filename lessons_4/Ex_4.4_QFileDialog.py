ce340cfe899b659ba995ea3220a4aab91437ad64
Евстафьев Александр Анатольевич <evstafev-aa@prima.local>
2022-12-06 12:57:01 +0300

    Ex_4.4

---
+++ lessons_4/Ex_4.4_QFileDialog.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QFileDialog, QApplication
+from PyQt5.QtGui import QIcon
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()  # возвращает родительский объект Example c классом
+
+        self.initUI()
+
+    def initUI(self):
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+        self.statusBar()
+
+        openFile = QAction(QIcon('open-svgrepo-com.svg'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open file')  # текст в статусбаре
+        openFile.triggered.connect(self.showDialog)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('My File Dialog')
+        self.show()
+
+    def showDialog(self):
+        fname = QFileDialog.getOpenFileNames(self, 'Open File', '/home')[0]
+
+        with open(fname, 'r') as f:
+            data = f.read()
+            self.textEdit.setText(data)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
